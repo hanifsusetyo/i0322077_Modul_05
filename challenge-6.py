c6039511d4e719d6d707b20b929865c9a074a9a9
hanifsusetyo <hanifputrasusetyo@student.uns.ac.id>
2023-04-03 12:02:00 +0700

    commit playground

---
+++ challenge-6.py
@@ -0,0 +1,5 @@
+daftar_belanja = ["gula", "garam", "beras", "kecap", "saus"]
+hari = ["senin", "selasa", "rabu", "kamis", "jumat", "sabtu"]
+
+for i in hari:
+    print("nopal belanja:")
---
+++ kamus.py
@@ -0,0 +1,8 @@
+kamus = ['arab', 'inggris', 'mandarin', 'jawa', 'jepang','jerman']
+
+responden = 10
+surveyor = 5
+
+while surveyor <= responden:
+    print("Jumlah surveyor =", surveyor)
+    surveyor += 1
